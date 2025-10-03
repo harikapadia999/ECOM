662c77016591305b09e05de197edeba41c5ff2ac
harikapadia999 <harikapadia999@gmail.com>
2025-10-03 15:22:16 +0530

    feat(payment-service): integrate Clerk authentication middleware and update test endpoint

--- apps/client/src/app/test/page.tsx
+++ apps/client/src/app/test/page.tsx
@@ -4,6 +4,8 @@ const TestPage = async () => {
   const { getToken } = await auth();
   const token = await getToken();
   //   console.log(token, "token from client");
+
+  //   Product Service
   const resProduct = await fetch("http://localhost:8000/test", {
     headers: {
       Authorization: `Bearer ${token}`,
@@ -12,6 +14,7 @@ const TestPage = async () => {
   const dataProduct = await resProduct.json();
   console.log(dataProduct, "data from product service");
 
+  //   Order Service
   const resOrder = await fetch("http://localhost:8001/test", {
     headers: {
       Authorization: `Bearer ${token}`,
@@ -19,6 +22,15 @@ const TestPage = async () => {
   });
   const dataOrder = await resOrder.json();
   console.log(dataOrder, "data from order service");
+
+  //   Payment Service
+  const resPayment = await fetch("http://localhost:8002/test", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const dataPayment = await resPayment.json();
+  console.log(dataPayment, "data from payment service");
   return <div>TestPage</div>;
 };
 
