9653d6d3a82b974b5459e5c46895fd0f6e9bf766
harikapadia999 <harikapadia999@gmail.com>
2025-10-05 14:51:39 +0530

    feat(product-service): implement update and delete functionality for products and categories

--- apps/product-service/controllers/product.controller.ts
+++ apps/product-service/controllers/product.controller.ts
@@ -25,8 +25,26 @@ export const createProduct = async (req: Request, res: Response) => {
 
   res.status(201).json(product);
 };
-export const updateProduct = async (req: Request, res: Response) => {};
-export const deleteProduct = async (req: Request, res: Response) => {};
+export const updateProduct = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const data: Prisma.ProductUpdateInput = req.body;
+
+  const updatedProduct = await prisma.product.update({
+    where: { id: Number(id) },
+    data,
+  });
+
+  return res.status(200).json({ product: updatedProduct });
+};
+export const deleteProduct = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const deletedProduct = await prisma.product.delete({
+    where: { id: Number(id) },
+  });
+
+  return res.status(200).json({ product: deletedProduct });
+};
 export const getProducts = async (req: Request, res: Response) => {
   const { sort, category, search, limit } = req.query;
 
@@ -62,4 +80,12 @@ export const getProducts = async (req: Request, res: Response) => {
 
   res.status(200).json(products);
 };
-export const getProduct = async (req: Request, res: Response) => {};
+export const getProduct = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const product = await prisma.product.findUnique({
+    where: { id: Number(id) },
+  });
+
+  return res.status(200).json(product);
+};
