4d8bafc51170baadf2927001cd8c4de092075989
harikapadia999 <harikapadia999@gmail.com>
2025-10-09 13:42:08 +0530

    feat(order-service, payment-service, product-service): add admin role verification middleware and update routes for admin access

--- apps/order-service/src/middleware/authMiddleware.ts
+++ apps/order-service/src/middleware/authMiddleware.ts
@@ -1,6 +1,6 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import Clerk from "@clerk/fastify";
-
+import type { CustomJwtSessionClaims } from "@repo/types";
 declare module "fastify" {
   interface FastifyRequest {
     userId?: string;
@@ -15,6 +15,22 @@ export const shouldBeUser = async (
   if (!userId) {
     return reply.status(401).send({ message: "You are not logged in!" });
   }
-
   request.userId = userId;
+};
+
+export const shouldBeAdmin = async (
+  request: FastifyRequest,
+  reply: FastifyReply
+) => {
+  const auth = Clerk.getAuth(request);
+  if (!auth.userId) {
+    return reply.status(401).send({ message: "You are not logged in!" });
+  }
+
+  const claims = auth.sessionClaims as CustomJwtSessionClaims;
+
+  if (claims.metadata?.role !== "admin") {
+    return reply.status(403).send({ message: "Unauthorized!" });
+  }
+  request.userId = auth.userId;
 };
