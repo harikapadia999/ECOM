83fea0e0cbbf83dec7ff2ff066ac235563668c78
harikapadia999 <harikapadia999@gmail.com>
2025-10-13 15:45:06 +0530

    feat(payment-service, product-service) :: implement type for product, cart and payment.
    Update necessary on client pages and components.

--- apps/payment-service/src/index.ts
+++ apps/payment-service/src/index.ts
@@ -1,7 +1,9 @@
+import { Product } from "./../../../packages/product-db/generated/prisma/index.d";
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { shouldBeUser } from "./middleware/authMiddleware.js";
+import stripe from "./utils/stripe.js";
 
 const app = new Hono();
 app.use("*", clerkMiddleware());
@@ -13,12 +15,24 @@ app.get("/health", (c) => {
     timestamp: Date.now(),
   });
 });
-app.get("/test", shouldBeUser, (c) => {
-  return c.json({
-    message: "Payment service is Authenticated",
-    userId: c.get("userId"),
-  });
-});
+// app.post("/create-stripe-product", async (c) => {
+//   const res = await stripe.products.create({
+//     id: "123",
+//     name: "Test Product",
+//     default_price_data: {
+//       currency: "usd",
+//       unit_amount: 10 * 100,
+//     },
+//   });
+//   return c.json(res);
+// });
+// app.get("/stripe-product-price", async (c) => {
+//   const res = await stripe.prices.list({
+//     product: "123",
+//   });
+
+//   return c.json(res);
+// });
 
 const start = async () => {
   try {
